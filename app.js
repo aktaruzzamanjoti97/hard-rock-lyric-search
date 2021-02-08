e4872b0bdf7138808c1a2dc7dda2478d4f69fa3c
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-02-08 20:14:36 +0600

    hard rock lyrics search

---
+++ app.js
@@ -0,0 +1,68 @@
+const searchSongs = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySongs(data.data))
+}
+
+
+// const searchSongs = async() => {
+//     const searchText = document.getElementById('search-field').value;
+//     const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+//     const res = await fetch(url)
+//     const data = await res.json();
+//     displaySongs(data.data);
+// }
+
+
+
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls preload="metadata">
+                <source src="${song.preview}" type="audio/ogg">
+            </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyrics('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>
+        `;
+        songContainer.appendChild(songDiv);
+    });
+}
+
+
+const getLyrics = (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLyrics(data.lyrics))
+};
+
+// const getLyrics = async(artist, title) => {
+//     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displayLyrics(data.lyrics);
+// };
+
+
+const displayLyrics = lyric => {
+    const lyricsContainer = document.getElementById('lyrics-container');
+    const lyricDiv = document.createElement('div');
+    lyricDiv.innerText = lyric;
+    lyricsContainer.appendChild(lyricDiv);
+}
+
+document.getElementById('search-button').addEventListener("click", function(){
+    const lyricsContainer = document.getElementById('lyrics-container');
+    lyricsContainer.innerText = '';
+})
